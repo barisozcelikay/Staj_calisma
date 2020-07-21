344794167fdeea8f8b61325cc2754527e886e9e5
barisozcelikay <ozcelikay.baris@gmail.com>
2020-07-21 20:09:16 +0300

    first commit

--- proje/src/Yazi.js
+++ proje/src/Yazi.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import  './Yazi.css';
+import Button from '@material-ui/core/Button';
+
 
 function Yazi({kelime}) {
     return (
         <div>
-            <h2>{kelime}</h2>
+            <ul>
+            <li><Button size="large" className="button" href = "https://www.turksat.com.tr/tr" target = "_blank">{kelime}</Button></li>
+            </ul>
+            
         </div>
     )
 }
