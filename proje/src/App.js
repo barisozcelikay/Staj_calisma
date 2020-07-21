b9db83801444d1bddbf98b6aa6ecc1f22c96e0aa
barisozcelikay <ozcelikay.baris@gmail.com>
2020-07-21 14:38:05 +0300

    first commit

---
+++ proje/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Yazi from './Yazi';
+
+function App() {
+  return (  
+    <div className="App">
+      <Yazi kelime = "Merhaba Dünya"/>
+      <Yazi kelime = "Hello World"/>
+      <Yazi kelime = "Привет мир"/>
+      <Yazi kelime = "Hola Mundo"/>
+      <Yazi kelime = "Hallo Welt"/>
+      <Yazi kelime = "안녕하세요 월드"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ proje/src/Yazi.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import  './Yazi.css';
+
+function Yazi({kelime}) {
+    return (
+        <div>
+            <h2>{kelime}</h2>
+        </div>
+    )
+}
+export default Yazi
